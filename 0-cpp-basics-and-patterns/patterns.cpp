b7bf7fe9fd1f12f62cfd874fae4012055358a179
Kashyap Sureshchandra Patel <kashyappatel.developer@gmail.com>
2026-01-27 22:18:39 +0530

    Worked on implementing patterns.

--- 0-cpp-basics-and-patterns/patterns.cpp
+++ 0-cpp-basics-and-patterns/patterns.cpp
@@ -174,6 +174,112 @@ int main() {
         }
         cout << endl;
     }
-    
+
+
+    // 1
+    // 01
+    // 101
+    // 0101
+    // 10101
+    for(int i = 0; i <= n; i++) {
+        if (i % 2 == 0) {
+            for(int j = 0; j < i; j++) {
+                if(j % 2 == 0) {
+                    cout << "0";
+                } else {
+                    cout << "1";
+                }
+            }
+            cout << endl;
+        } else {
+            for(int j = 0; j < i; j++) {
+                if(j % 2 == 0) {
+                    cout << "1";
+                } else {
+                    cout << "0";
+                }
+            }
+            cout << endl;
+        }
+    }
+
+    // 1      1
+    // 12    21
+    // 123  321
+    // 12344321
+    int spaces = 2*(n - 1);
+    for(int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << j + 1;
+        }
+
+        for (int j = 0; j < spaces; j++) {
+            cout << " ";
+        }
+        
+        for (int j = i; j > 0; j--) {
+            cout << j;
+        }
+        
+        spaces -= 2;
+        cout << endl;
+    }
+
+    // 1 
+    // 2 3 
+    // 4 5 6 
+    // 7 8 9 10 
+    // 11 12 13 14 15 
+    int temp = 1;
+    for(int i = 0; i <= n; i++) {
+        for(int j = 0; j < i; j ++) {
+            cout << temp << " ";
+            temp ++;
+        }
+        cout << endl;
+    }
+
+    // A
+    // AB
+    // ABC
+    // ABCD
+    // ABCDE
+    for(int i = 0; i <= n; i++) {
+        temp = 65;
+        for(int j = 0; j < i; j++) {
+            cout << char(temp);
+            temp ++;
+        }
+        cout << endl;
+    }
+
+    // ABCDE
+    // ABCD
+    // ABC
+    // AB
+    // A
+    for(int i = 0; i <= n; i++) {
+        temp = 65;
+        for(int j = n; j > i; j--) {
+            cout << char(temp);
+            temp ++;
+        }
+        cout << endl;
+    }
+
+    // A
+    // BB
+    // CCC
+    // DDDD
+    // EEEEE
+    temp = 65;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= i; j++) {
+            cout << char(temp);
+        }
+        temp ++;
+        cout << endl;
+    }
+
     return 0;
 }
